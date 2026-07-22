34322e98b2578c97d1c6c4b4ee16fa3d1597453a
Kinkaki <144636023+Kinkaki@users.noreply.github.com>
2026-07-22 20:52:06 +0700

    Update KernelSU setup script URL

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/KOWX712/KernelSU/master/kernel/setup.sh" | bash
 
 # Grab AnyKernel3 if it isn't already here (GitHub Actions clones it in a
 # separate step; this is just a fallback for local builds)
